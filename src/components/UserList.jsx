762ac4c8b4d317158f39b2bc4d3f48bf22e49396
RickyDan <tangzhiming90@gmail.com>
2019-03-11 11:48:20 +0800

    使用hooks替代class组件

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
-import { Button, Table } from 'antd'
+import React, { useEffect } from 'react'
+import { Table } from 'antd'
 
 const columns = [{
+  title: 'id',
+  dataIndex: 'id',
+  key: 'id'
+}, {
   title: '用户名',
   dataIndex: 'username',
   key: 'username'
@@ -10,14 +14,18 @@ const columns = [{
   dataIndex: 'nickname',
   key: 'nickname'
 }, {
-  title: '权限',
-  dataIndex: 'auth',
-  key: 'auth'
+  title: '创建时间',
+  dataIndex: 'createdAt',
+  key: 'createdAt'
 }]
-const UserList = ({ users, fetchUser }) => (
-  <div>
-    <Table dataSource={users} columns={columns} />
-    <Button onClick={fetchUser}>Click me</Button>
-  </div>
-)
+const UserList = ({ users, fetchUser }) => {
+  useEffect(() => {
+    fetchUser()
+  }, [])
+  return (
+    <div>
+      <Table dataSource={users} columns={columns} rowKey="id" bordered />
+    </div>
+  )  
+}
 export default UserList
