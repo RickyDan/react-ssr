37ff6c09e03852e2d05702515a043a8655d594ed
RickyDan <tangzhiming90@gmail.com>
2019-03-15 13:41:29 +0800

    集成sentry前端异常监控

---
+++ src/components/SentryBoundary.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Raven from 'raven-js'
+
+export default class SentryBoundary extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { error: null }
+  }
+
+  componentDidCatch(error, errorInfo) {
+    this.setState({ error })
+    // 发送错误消息
+    Raven.captureException(error, { extra: errorInfo })
+  }
+
+  render() {
+    if (this.state.error) {
+      console.log('React Error')
+    }
+    return this.props.children
+  }
+}
--- src/index.js
+++ src/index.js
@@ -4,19 +4,24 @@ import { createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 import createSagaMiddleware from 'redux-saga'
 import { Provider } from 'react-redux'
+import Raven from 'raven-js'
+import SentryBoundary from './components/SentryBoundary'
 import rootState from './reducers'
 import rootSaga from './sagas'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import './assets/styles/index.less'
 
+Raven.config('https://e31a17efa7d7479fa66e8d0835a3a4cf@sentry.io/1416058').install()
 const sagaMiddleware = createSagaMiddleware()
 const middlewares = [sagaMiddleware, logger]
 const store = createStore(rootState, applyMiddleware(...middlewares))
 sagaMiddleware.run(rootSaga)
 render(
   <Provider store={store}>
-    <App />
+    <SentryBoundary>
+      <App />
+    </SentryBoundary>
   </Provider>,
   document.getElementById('root')
 )
